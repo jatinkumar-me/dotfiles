ae1d784325f7fa69c5c9c127b441fd0bebe67fd3
jatinkumar-me <jatin208015@gmail.com>
2023-10-23 16:40:06 +0530

    added new config files

---
+++ i3/.config/i3/power-menu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rofi -show p -modi p:'/home/jatin/.config/i3/rofi-power-menu.sh --symbols-font "Symbols Nerd Font Mono"' -theme-str 'window {width: 20em;} listview {lines: 6;}'
