7d895a9ade968eb671a8ba38019d6a57988376ed
Jatin Kumar <jatinkumar-btp@Jatins-MacBook-Air.local>
2023-12-08 12:55:05 +0530

    modified nvim-tree config
    enabled diagnostic in nvim-tree and changed min and max-width

--- nvim/.config/nvim/lua/core/plugins-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugins-config/nvim-tree.lua
@@ -3,13 +3,27 @@ vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
   view = {
-    adaptive_size = true
+    adaptive_size = true,
+    width = {
+      min = '10%',
+      max = '20%'
+    }
   },
   update_cwd = true,
   update_focused_file = {
     enable = true,
     update_cwd = true
   },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true
+  },
+  filters = {
+    git_ignored = false
+  },
+  system_open = {
+    cmd = "open"
+  }
 })
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
