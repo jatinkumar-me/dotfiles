f10e49b70cd750afad2c5c6f2ede0d002dbfef44
jatinkumar-me <jatin208015@gmail.com>
2024-01-31 14:35:18 +0530

    battery script change

--- scripts/battery-warning.sh
+++ scripts/battery-warning.sh
@@ -4,7 +4,7 @@ battery_stat="$(acpi --battery)"
 battery_greped_status="$(echo $battery_stat | grep -Pio 'remaining|charged')"
 battery_percentage_v="$(echo $battery_stat | grep -Po '(\d+%)' | grep -Po '\d+')"
 
-if [ "$battery_greped_status" == "remaining" ] && [ "$battery_percentage_v" -lt 15 ]; then
+if [ "$battery_greped_status" == "remaining" ] && [ "$battery_percentage_v" -lt 30 ]; then
     runtime="$(echo $battery_stat | grep -Po '[0-9]+:[0-9]+:[0-9]+')"
     dunstify -a system -i "/usr/share/icons/Arc/panel/16/battery-caution.svg" -t 0 -r 9990 -u critical "Battery Critically Low" "${battery_percentage_v}% Remaining.\nRuntime: $runtime"
 fi
