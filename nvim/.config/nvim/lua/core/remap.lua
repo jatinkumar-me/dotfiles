07c3af961afbce188a0201e33fbcb6be5796443e
jatinkumar-me <jatin208015@gmail.com>
2024-02-12 00:04:37 +0530

    formatting fix

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -27,14 +27,14 @@ vim.keymap.set("n", "<leader>h", "<cmd>noh<CR>", { desc = "No highights" });
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-FormatBuffer = function ()
+FormatBuffer = function()
   vim.lsp.buf.format({
     timeout_ms = 2000
   })
 end
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", FormatBuffer, { desc = "Format document" })
-vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "[C]ode [A]ction"})
+vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { desc = "[C]ode [A]ction" })
 
 -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -55,10 +55,10 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Focus on windows
--- vim.keymap.set('n', '<C-h>', '<C-w>h')
--- vim.keymap.set('n', '<C-l>', '<C-w>l')
--- vim.keymap.set('n', '<C-j>', '<C-w>j')
--- vim.keymap.set('n', '<C-k>', '<C-w>k')
+-- vim.keymap.set('n', '<A-h>', '<C-w>h')
+-- vim.keymap.set('n', '<A-l>', '<C-w>l')
+-- vim.keymap.set('n', '<A-j>', '<C-w>j')
+-- vim.keymap.set('n', '<A-k>', '<C-w>k')
 
 -- Open up a link in the browser
 HandleURL = function()
@@ -71,20 +71,22 @@ HandleURL = function()
 end
 
 -- vim.keymap.set({ "n", "v" }, "gx", [[:!xdg-open <C-r><C-a><CR>]], { desc = "Open the link the browser" })
-vim.keymap.set( "n", "gx", [[<Cmd>lua HandleURL()<CR>]], { desc = "Open the link the browser" })
+vim.keymap.set("n", "gx", [[<Cmd>lua HandleURL()<CR>]], { desc = "Open the link the browser" })
 
 -- Jump between buffers
-vim.keymap.set('n', 'th', [[<Cmd>bprev<CR>]], { desc = "Jump to previous buffer"})
-vim.keymap.set('n', 'tl', [[<Cmd>bnext<CR>]], { desc = "Jump to next buffer"})
-vim.keymap.set('n', 'tc', [[<Cmd>bdelete<CR>]], { desc = "Jump to next buffer"})
+vim.keymap.set('n', 'th', [[<Cmd>bprev<CR>]], { desc = "Jump to previous buffer" })
+vim.keymap.set('n', 'tl', [[<Cmd>bnext<CR>]], { desc = "Jump to next buffer" })
+vim.keymap.set('n', 'tc', [[<Cmd>bdelete<CR>]], { desc = "Close current buffer" })
 
--- Toggle nvim illuminate 
-vim.keymap.set('n', '<leader>k', [[<Cmd>IlluminateToggle<CR>]], { desc = "Disable LSP Highlights"})
+-- Toggle nvim illuminate
+vim.keymap.set('n', '<leader>k', [[<Cmd>IlluminateToggle<CR>]], { desc = "Disable LSP Highlights" })
 
 -- Go to next reference.
-vim.keymap.set('n', '<leader>n', [[<Cmd>lua require('illuminate').goto_next_reference()<CR>]], { desc = "Go to next reference"})
+vim.keymap.set('n', '<leader>n', [[<Cmd>lua require('illuminate').goto_next_reference()<CR>]],
+{ desc = "Go to next reference" })
 -- Go to previous reference
-vim.keymap.set('n', '<leader>N', [[<Cmd>lua require('illuminate').goto_prev_reference()<CR>]], { desc = "Go to previous reference"})
+vim.keymap.set('n', '<leader>N', [[<Cmd>lua require('illuminate').goto_prev_reference()<CR>]],
+{ desc = "Go to previous reference" })
 
 vim.keymap.set('t', '<C-\\>', '<C-\\><C-n>')
 vim.keymap.set('t', '<A-h>', '<C-\\><C-n><C-w>h')
