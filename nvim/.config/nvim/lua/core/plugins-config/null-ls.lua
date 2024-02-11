ab474f2971e26d467f0671313c4a3133458182f4
jatinkumar-me <jatin208015@gmail.com>
2024-02-12 00:02:01 +0530

    enable prettier

--- nvim/.config/nvim/lua/core/plugins-config/null-ls.lua
+++ nvim/.config/nvim/lua/core/plugins-config/null-ls.lua
@@ -4,7 +4,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     -- javascript/typescript
-    -- null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettier,
     -- null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.eslint,
     -- golang
