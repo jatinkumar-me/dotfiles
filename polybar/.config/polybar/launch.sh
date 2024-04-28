4a336de1ba0e6cb29e286e92ce22d5c6f4c20c0e
jatinkumar-me <jatin208015@gmail.com>
2024-04-28 13:10:10 +0530

    polybar multi monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,7 +5,15 @@
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 
-echo "---" | tee -a /tmp/mybar.log
-polybar mybar 2>&1 | tee -a /tmp/mybar.log & disown
+# echo "---" | tee -a /tmp/mybar.log
+# polybar mybar 2>&1 | tee -a /tmp/mybar.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bar launched..."
