757a90983e870e6db1f89d8985fefaaea5194450
jatinkumar-me <jatin208015@gmail.com>
2024-07-31 16:36:06 +0530

    polybar taskwarrior change

--- polybar/.config/polybar/taskwarrior.sh
+++ polybar/.config/polybar/taskwarrior.sh
@@ -7,4 +7,4 @@ most_urgent_desc=`task rc.verbose: rc.report.next.columns:description rc.report.
 most_urgent_id=`task rc.verbose: rc.report.next.columns:id rc.report.next.labels:1 limit:1 next`
 most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
 echo "$most_urgent_id" > /tmp/tw_polybar_id
-echo "$most_urgent_desc ·  $most_urgent_due"
+echo "$most_urgent_due $most_urgent_desc"
