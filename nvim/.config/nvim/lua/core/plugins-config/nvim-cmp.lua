d61b3fec4ffede8c60101f7986ff172fcced93c0
jatinkumar-me <jatin208015@gmail.com>
2024-08-02 21:14:15 +0530

    snippet keymap change
    uncomment the keybinding

--- nvim/.config/nvim/lua/core/plugins-config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/core/plugins-config/nvim-cmp.lua
@@ -18,7 +18,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    --[[ ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<A-Space>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
@@ -27,7 +27,7 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    ['<S-A-Space>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -35,7 +35,7 @@ cmp.setup {
       else
         fallback()
       end
-    end, { 'i', 's' }), ]]
+    end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
