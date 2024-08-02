4dbfafb455adcfd6248dc215329f02f063eaee9d
jatinkumar-me <jatin208015@gmail.com>
2024-08-02 21:13:32 +0530

    add .project to find root

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -39,7 +39,7 @@ local config = {
   },
   -- This is the default if not provided, you can remove it. Or adjust as needed.
   -- One dedicated LSP server & client will be started per unique root_dir
-  root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'pom.xml', 'build.gradle' }),
+  root_dir = require('jdtls.setup').find_root({'.project', 'mvnw', 'pom.xml', 'build.gradle' }),
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   settings = {
