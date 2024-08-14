602299ac31f66ecb3a323b8f1d87bc7ac6a74966
jatinkumar-me <jatin208015@gmail.com>
2024-08-14 23:01:36 +0530

    nvim-tree netrw bug fix

--- nvim/.config/nvim/lua/core/plugins-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugins-config/nvim-tree.lua
@@ -13,6 +13,13 @@ require("nvim-tree").setup({
     enable = true,
     update_cwd = true
   },
+  disable_netrw = false,
+  hijack_netrw = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = false,
+  },
 })
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
