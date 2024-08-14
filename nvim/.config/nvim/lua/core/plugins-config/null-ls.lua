fc35af686e46d2384d88e8863ae344d9cbe61ecc
jatinkumar-me <jatin208015@gmail.com>
2024-08-14 23:01:28 +0530

    null ls vale

--- nvim/.config/nvim/lua/core/plugins-config/null-ls.lua
+++ nvim/.config/nvim/lua/core/plugins-config/null-ls.lua
@@ -6,13 +6,14 @@ null_ls.setup({
     -- javascript/typescript
     -- null_ls.builtins.formatting.prettier,
     -- null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.vale,
     null_ls.builtins.formatting.eslint,
     -- golang
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.latexindent,
-    null_ls.builtins.formatting.beautysh
+    null_ls.builtins.formatting.beautysh,
   },
   --[[ on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
