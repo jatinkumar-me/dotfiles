03a85e613ab05f3a1238128c1530216514465608
jatinkumar-me <jatin208015@gmail.com>
2024-08-23 15:28:48 +0530

    telescope root folder

--- nvim/.config/nvim/lua/core/plugins-config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins-config/telescope.lua
@@ -5,12 +5,12 @@ require('telescope').setup {
     file_ignore_patterns = {
       "node_modules"
     },
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
+    -- mappings = {
+    --   i = {
+    --     ['<C-u>'] = false,
+    --     ['<C-d>'] = false,
+    --   },
+    -- },
   },
 }
 
@@ -30,8 +30,8 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', function() builtin.find_files({ cwd = vim.fn.expand('%:p:h') }) end,
-  { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function() builtin.find_files() end, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sl', function() builtin.find_files({ cwd = vim.fn.expand('%:p:h') }) end, { desc = '[S]earch Files [L]ocally' })
 vim.keymap.set('n', '<leader>ss', builtin.git_files, { desc = '[S]earch git files' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
