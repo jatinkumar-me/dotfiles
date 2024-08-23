00acae7835427d1e46e3fc2074894cfd345ceb73
jatinkumar-me <jatin208015@gmail.com>
2024-08-23 15:30:00 +0530

    visual model refactoring lsp

--- nvim/.config/nvim/lua/core/plugins-config/lsp-config.lua
+++ nvim/.config/nvim/lua/core/plugins-config/lsp-config.lua
@@ -17,6 +17,7 @@ local on_attach = function(_, bufnr)
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  vim.keymap.set('v', '<leader>c', vim.lsp.buf.code_action, { buffer = bufnr, desc = 'Action' })
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
