54249041261f36f08b1d524a13bf56ece25c9ea4
Jatin Kumar <jatin208015@gmail.com>
2024-09-02 15:41:23 +0530

    mouse name change

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -5,8 +5,8 @@ redshift -P -O 3400
 
 # Disable touchpad, change mouse sensitivity
 xinput disable 'Elan Touchpad'
-xinput set-prop 'USB Optical Mouse' 'libinput Accel Speed' -0.5
-xinput set-prop 'USB Optical Mouse' 'libinput Accel Profile Enabled' 0, 1
+xinput set-prop 'YSPRINGTECH USB OPTICAL MOUSE' 'libinput Accel Speed' 0.0
+xinput set-prop 'YSPRINGTECH USB OPTICAL MOUSE' 'libinput Accel Profile Enabled' 0, 1
 
 # Fixing screen tear
 xrandr --output eDP --set TearFree on
