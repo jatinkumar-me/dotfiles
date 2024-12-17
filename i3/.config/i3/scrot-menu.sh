3f6cf0bf88a67815959674673a2f32cf5516e15e
Jatin Kumar <jatin208015@gmail.com>
2024-12-17 11:51:47 +0530

    scrot ocr feature

--- i3/.config/i3/scrot-menu.sh
+++ i3/.config/i3/scrot-menu.sh
@@ -4,11 +4,12 @@
 option0="󰹑  screen"
 option1="  area"
 option2="  window"
+option3="ocr clipboard"
 
 # options to be displyed
-options="$option0\n$option1\n$option2"
+options="$option0\n$option1\n$option2\n$option3"
 
-selected="$(echo -e "$options" | rofi -dmenu -theme-str 'window {width: 20em;} listview {lines: 3;}' -p "scrot")"
+selected="$(echo -e "$options" | rofi -dmenu -theme-str 'window {width: 20em;} listview {lines: 4;}' -p "scrot")"
 case $selected in
     $option0)
         cd ~/Pictures/scrots/ && sleep 1 && scrot --silent;;
@@ -16,4 +17,6 @@ case $selected in
         cd ~/Pictures/scrots/ && scrot -s -f --silent;;
     $option2)
         cd ~/Pictures/scrots/ && sleep 1 && scrot -u --silent;;
+    $option3)
+        cd ~/Pictures/scrots/ && sleep 1 && scrot --silent -s -f -F - | tesseract stdin stdout | xclip -selection clipboard;;
 esac
